55401bca221692931deb20aa452b9d3402abf742
EC2 Default User <120617980+zvyujg@users.noreply.github.com>
2023-02-04 10:06:15 +0000

    Initial Commit

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,22 @@
+class BookCommentsController < ApplicationController
+    
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to book_path(book)
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: 'homes#top'
+  get "home/about"=>"homes#about"
+  
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+   resource :favorites, only: [:create, :destroy]
+   resources :book_comments, only: [:create]
+  end
+  resources :users, only: [:index,:show,:edit,:update]
+  
+  
+  
+end
